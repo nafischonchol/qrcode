5459f57f1a50c0ed823b6eb10bcae80712becf61
nafischonchol <nafisislam161@gmail.com>
2021-03-24 09:12:48 +0600

    Qr Code Using Google Api

--- index.php
+++ index.php
@@ -1,29 +1,15 @@
 <?php
 
-    include('phpqrcode/qrlib.php');
-    // include('config.php');
-
-    // how to save PNG codes to server
+    
     
     $tempDir = "qrimg/";
     
-    $codeContents = "ABL-0000-1111#ABC-0000-1111#ABL Head";
-    
-    // we need to generate filename somehow, 
-    // with md5 or with database ID used to obtains $codeContents...
-    $fileName = 'ABL-0000-1111-1.png';
-    
-    $pngAbsoluteFilePath = $tempDir.$fileName;
-    $urlRelativeFilePath = $tempDir.$fileName;
-    
-    QRcode::png($codeContents, $pngAbsoluteFilePath);
-    echo 'File generated! '.$codeContents;
-    echo '<hr />';
-    
-    echo 'Server PNG File: '.$pngAbsoluteFilePath;
-    echo '<hr />';
-    
-    // displaying
-    echo '<img src="'.$urlRelativeFilePath.'" />';
+    $codeContents = "Nafis Chonchol#Tangail#ABL Head";
+    $codeContents=urlencode($codeContents);
+    $fileName = 'naf.jpg';
+    $save=$tempDir.$fileName;
+    $qr="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=".$codeContents;
+    copy($qr,$save);
+    header("lOCATION:".$qr);
     
 ?>
